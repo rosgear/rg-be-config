d3b0cba9a0126372a390802d112aa175a13f3449
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 18:18:38 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Configuration',
+    '{description}' => 'System configuration',
+    '{permissions}' => [
+        'any'       => ['Full access', 'Access to system configurations'],
+        'extension' => ['Extension', 'Access to extensions (system configurations)']
+    ],
+];
