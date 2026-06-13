4954d4c935f0f969e4a1dc90cfae516d2a5e203b
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 18:18:25 +0300

    Файл конфигурации версии модуля

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии модуля.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Configuration',
+    'description' => 'System configuration',
+    'version'     => '1.0',
+    'versionDate' => '10-06-2025',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-config/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
